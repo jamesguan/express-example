ee7ed7b343463d641f5f2950cbd6e5b26186bb0d
James Guan <james.guan@capitalone.com>
2022-04-08 15:15:02 -0400

    [JAMES] - First commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+   res.status(200).json({
+       message: 'Helloworld',
+   }) 
+});
+
+const dogsList = [];
+
+app.post('/dogs', (req, res) => {
+    const { dogName } = req.body;
+    console.log(req.body);
+    dogsList.push(dogName);
+    res.status(204).send();
+});
+
+app.get('/dogs', (req, res) => {
+    return res.json(dogsList);
+});
+
+const server = app.listen(9001, (err) => {
+    if (err) {
+        console.error('Error starting server');
+        throw err;
+    }
+
+    console.log('Started on port 9001');
+});
+
+
+/*
+example(3000, () => { console.log('James')});
+
+function example (timeout, func) {
+    console.log(timeout);
+    setTimeout(func, timeout);
+};
+*/
